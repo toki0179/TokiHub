4054bf66a8a88424b74e49c858220df052b96a48
toki0179 <47860095+toki0179@users.noreply.github.com>
2021-03-23 20:42:27 +0000

    Add files via upload

---
+++ script.js
@@ -0,0 +1,24 @@
+var countDownDate = new Date("Mar 27, 2021 16:00:00").getTime();
+
+// Run myfunc every second
+var myfunc = setInterval(function() {
+  
+  var now = new Date().getTime();
+  var timeleft = countDownDate - now;
+        
+  // Calculating the days, hours, minutes and seconds left
+  var days = Math.floor(timeleft / (1000 * 60 * 60 * 24));
+  var hours = Math.floor((timeleft % (1000 * 60 * 60 * 24)) / (1000 * 60 * 60));
+  var minutes = Math.floor((timeleft % (1000 * 60 * 60)) / (1000 * 60));
+  var seconds = Math.floor((timeleft % (1000 * 60)) / 1000);
+        
+  // Result is output to the specific element
+  document.getElementById("time").innerHTML = days + "d " + hours + "h "
+  + minutes + "m " + seconds + "s ";
+        
+  // Display the message when countdown is over
+  if (timeleft < 0) {
+    clearInterval(myfunc);
+    document.getElementById("time").innerHTML = "TIME UP!!";
+  }
+})
